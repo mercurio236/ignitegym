e246ba22c958d1d2ccd811df0dee0be7c5883b6a
Arley Souto <macx15@hotmail.com>
2023-06-13 00:05:08 -0300

    adicionando os tokens de cores e tamanhos

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@
 import { NativeBaseProvider } from 'native-base'
 import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,14 +13,14 @@ export default function App() {
 
   return (
 
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
       {
-        fontsLoaded ? <View /> : <View />
+        fontsLoaded ? <View /> : <Loading />
       }
     </NativeBaseProvider>
   );
