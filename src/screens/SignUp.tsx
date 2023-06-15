48b13555ef5f75c591c38055d5236c6dfce283ed
Arley Souto <macx15@hotmail.com>
2023-06-14 23:14:01 -0300

    adicionando defaultSource nas imagens

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -21,6 +21,7 @@ export function SignUp() {
             <VStack flex={1}  px={10} pb={16}>
                 <Image
                     source={BackgroundImgs}
+                    defaultSource={BackgroundImgs}
                     alt='Pessoas treinando'
                     resizeMode='contain'
                     position='absolute'
