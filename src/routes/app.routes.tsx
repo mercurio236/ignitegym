d7ac4838ae3a53ac06a4f8847d52431b52fd2c68
Arley Souto <macx15@hotmail.com>
2023-06-15 22:00:35 -0300

    tipagem do app routes table

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,20 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 import { Home } from '@screens/Home';
 import { Exercise } from '@screens/Exercise';
 import { Profile } from '@screens/Profile';
 import { History } from '@screens/History';
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+    home:undefined;
+    exercise:undefined;
+    profile:undefined;
+    history:undefined
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 
 export function AppRoutes() {
@@ -16,7 +25,7 @@ export function AppRoutes() {
                 component={Home}
             />
             <Screen
-                name='hitory'
+                name='history'
                 component={History}
             />
             <Screen
