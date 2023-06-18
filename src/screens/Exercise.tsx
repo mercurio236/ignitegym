861eb98898ab2258680a58cc24b91ff014d0b96f
Arley Souto <macx15@hotmail.com>
2023-06-18 20:02:56 -0300

    ajustado header usando o flexShrink

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,14 +1,16 @@
-import { Center, Icon, Text, VStack } from 'native-base'
+import { Center, HStack, Heading, Icon, Text, VStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
-import {Feather} from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
     const navigation = useNavigation<AppNavigatorRoutesProps>()
 
-    function handleGoback(){
-        navigation.goBack()    
+    function handleGoback() {
+        navigation.goBack()
     }
 
     return (
@@ -16,12 +18,25 @@ export function Exercise() {
             <VStack px={8} bg='gray.600' pt={12}>
                 <TouchableOpacity onPress={handleGoback}>
                     <Icon
-                    as={Feather}
-                    name='arrow-left'
-                    color='green.500'
-                    size={6}
+                        as={Feather}
+                        name='arrow-left'
+                        color='green.500'
+                        size={6}
                     />
                 </TouchableOpacity>
+
+                <HStack justifyContent='space-between' mt={4} mb={8} alignItems='center' >
+                    <Heading color='gray.100' fontSize='lg' flexShrink={1}>
+                        Puxada frontal
+                    </Heading>
+
+                    <HStack alignItems='center'>
+                        <BodySvg/>
+                        <Text color='gray.200' ml={1} textTransform='capitalize'>
+                            Costas
+                        </Text>
+                    </HStack>
+                </HStack>
             </VStack>
         </VStack>
     )
