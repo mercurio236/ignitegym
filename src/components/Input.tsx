a823f058e9467c8f4975fa2020b67ba665579e56
Arley Souto <macx15@hotmail.com>
2023-06-19 23:13:23 -0300

    mensagem de error e nova tipagem no component de input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,33 @@
-import { Input as NativeBseInput, IInputProps } from 'native-base'
+import { Input as NativeBseInput, IInputProps, FormControl } from 'native-base'
 
-export function Input({ ...rest }: IInputProps) {
+type Props = IInputProps & {
+    errorMessage?: string | null;
+}
+
+export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
+    const invalid = !!errorMessage || isInvalid;
+
     return (
-        <NativeBseInput
-            bg='gray.700'
-            h={14}
-            px={4}
-            borderWidth={0}
-            fontSize='md'
-            color='white'
-            fontFamily='body'
-            mb={4}
-            placeholderTextColor='gray.300'
-            _focus={{
-                bg:'gray.700',
-                borderWidth:1,
-                borderColor:'green.500'
-            }}
-            {...rest}
-        />
+        <FormControl isInvalid={invalid} mb={4}>
+            <NativeBseInput
+                bg='gray.700'
+                h={14}
+                px={4}
+                borderWidth={0}
+                fontSize='md'
+                color='white'
+                fontFamily='body'
+                placeholderTextColor='gray.300'
+                _focus={{
+                    bg: 'gray.700',
+                    borderWidth: 1,
+                    borderColor: 'green.500'
+                }}
+                {...rest}
+            />
+            <FormControl.ErrorMessage>
+                {errorMessage}
+            </FormControl.ErrorMessage>
+        </FormControl>
     )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,7 +20,7 @@ type FormDataProps = {
 
 export function SignUp() {
 
-    const { control, handleSubmit } = useForm<FormDataProps>({
+    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         defaultValues: {
             name: '',
             email: '',
@@ -62,12 +62,15 @@ export function SignUp() {
                     <Controller
                         control={control}
                         name='name'
-
+                        rules={{
+                            required: 'Informe o nome'
+                        }}
                         render={({ field: { onChange, value } }) => (
                             <Input
                                 placeholder='Nome'
                                 onChangeText={onChange}
                                 value={value}
+                                errorMessage={errors.name?.message}
                             />
                         )}
                     />
@@ -75,6 +78,13 @@ export function SignUp() {
                     <Controller
                         control={control}
                         name='email'
+                        rules={{
+                            required: 'Informe o email',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'E-mail inválido'
+                            }
+                        }}
                         render={({ field: { onChange, value } }) => (
                             <Input
                                 placeholder='E-mail'
@@ -82,9 +92,12 @@ export function SignUp() {
                                 autoCapitalize='none'
                                 onChangeText={onChange}
                                 value={value}
+                                errorMessage={errors.email?.message}
                             />
                         )}
                     />
+
+
                     <Controller
                         control={control}
                         name='password'
