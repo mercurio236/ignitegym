f15f54b710536050d796f844846c6ccd36483d18
Arley Souto <macx15@hotmail.com>
2023-06-19 23:50:19 -0300

    ajusto do botão de voltar para login

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -140,7 +140,7 @@ export function SignUp() {
                 <Button
                     title='Voltar para o login'
                     variant='outlined'
-                    mt={24}
+                    mt={12}
                     onPress={handleGoBack}
                 />
 
