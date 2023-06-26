3cee8489a6c09403b71d1230b81af321d0530fea
Arley Souto <macx15@hotmail.com>
2023-06-25 22:09:42 -0300

    adicinando nomes dos exercicios e imagens

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -2,16 +2,20 @@ import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base'
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 
-type Props = TouchableOpacityProps & {
+import { api } from '@services/api'
 
+import { ExerciseDTO } from '@dtos/ExerciseDTO'
+
+type Props = TouchableOpacityProps & {
+    data: ExerciseDTO
 }
 
-export function ExerciseCard({ ...rest }: Props) {
+export function ExerciseCard({data, ...rest }: Props) {
     return (
         <TouchableOpacity {...rest}>
             <HStack bg='gray.500' alignItems='center' p={2} pr={4} rounded='md' mb={3}>
                 <Image
-                    source={{ uri: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+                    source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
                     alt='Imagem do exercício'
                     w={16}
                     h={16}
@@ -21,10 +25,10 @@ export function ExerciseCard({ ...rest }: Props) {
                 />
                 <VStack flex={1}>
                     <Heading fontSize='lg' color='white' fontFamily='heading'>
-                        Remada unilateral
+                        {data.name}
                     </Heading>
                     <Text fontSize='sm' color='gray.200' mt={1} numberOfLines={2}>
-                        3 séries x 12 repetições
+                        {data.series} séries x {data.repetitions} repetições
                     </Text>
                 </VStack>
                 <Icon
