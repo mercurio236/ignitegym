212995864e89524084b147d6d7fce78ea52b5aa7
Arley Souto <macx15@hotmail.com>
2023-06-30 00:01:53 -0300

    armazenando refresh_token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 import { AppError } from "@utils/AppError";
 import axios, { AxiosInstance } from "axios";
 
@@ -12,11 +13,28 @@ const api = axios.create({
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = signOut => {
-    const interceptTokenManager = api.interceptors.response.use(response => response, error => {
-        if (error.response && error.response.data) {
-            return Promise.reject(new AppError(error.response.data.message));
+    const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
+        if (requestError?.response?.status === 401) {
+            if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+                const { refresh_token } = await storageAuthTokenGet()
+
+                if (!refresh_token) {
+                    signOut();
+                    return Promise.reject(requestError);
+                }
+            }
+
+            signOut();
+        }
+
+
+
+
+
+        if (requestError.response && requestError.response.data) {
+            return Promise.reject(new AppError(requestError.response.data.message));
         } else {
-            return Promise.reject(error)
+            return Promise.reject(requestError)
         }
     })
 
