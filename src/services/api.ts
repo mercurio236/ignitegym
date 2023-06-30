e0431f1250add69ca48075e6eb754e753df63549
Arley Souto <macx15@hotmail.com>
2023-06-30 00:13:53 -0300

    fila de requisição implementada

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,13 @@
 import { storageAuthTokenGet } from "@storage/storageAuthToken";
 import { AppError } from "@utils/AppError";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 type SignOut = () => void
+
+type PromiseType = {
+    onSuccess: (token: string) => void;
+    onFailure: (error: AxiosError) => void;
+}
 
 type APIInstanceProps = AxiosInstance & {
     registerInterceptTokenManager: (signOut: SignOut) => () => void
@@ -11,6 +16,9 @@ type APIInstanceProps = AxiosInstance & {
 const api = axios.create({
     baseURL: 'http://192.168.0.11:3333'
 }) as APIInstanceProps;
+
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
 
 api.registerInterceptTokenManager = signOut => {
     const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
@@ -22,6 +30,26 @@ api.registerInterceptTokenManager = signOut => {
                     signOut();
                     return Promise.reject(requestError);
                 }
+
+                const originalRequestConfig = requestError.config;
+
+                if (isRefreshing) {
+                    return new Promise((resolve, reject) => {
+                        failedQueue.push({
+                            onSuccess: (token: string) => {
+                                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}` };
+                                resolve(api(originalRequestConfig))
+                            },
+                            onFailure: (error: AxiosError) => {
+                                reject(error)
+                            },
+                        })
+                    })
+                }
+
+                isRefreshing = true;
+
+
             }
 
             signOut();
